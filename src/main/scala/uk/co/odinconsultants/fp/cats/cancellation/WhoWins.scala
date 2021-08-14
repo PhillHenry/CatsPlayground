b3d3a53b9412691569a0931ca1a8de2e007c26d0
Phillip Henry <PhillHenry@gmail.com>
2021-08-14 11:32:06 +0100

    Refactored but interruptible does not appear to work.

--- src/main/scala/uk/co/odinconsultants/fp/cats/cancellation/WhoWins.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/cancellation/WhoWins.scala
@@ -7,15 +7,21 @@ import cats.implicits._
 
 object WhoWins extends IOApp {
 
-  def doSomething(x: Int): IO[Unit] = IO.println(s"Starting x = $x") >>
-    IO.sleep(1.seconds) >>
-    IO.println(s"Finished with $x")
+  def print(x: Any): IO[Unit] =
+    IO.println(s"$x").onCancel(IO.println(s"$x cancelled")).onError(t => IO {t.printStackTrace()})
+
+  def doSomething(x: Int): IO[Unit] =
+    print(s"Starting $x") >>
+    IO.sleep(1.seconds).onCancel(IO.println(s"$x sleep cancelled")) >>
+    print(s"Finished $x")
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val something = for {
+    val something: IO[Any] = for {
       first <- doSomething(1).start
       _ <- first.cancel
+      interruptible <- IO.interruptible(many = false)(doSomething(3)).start
       second <- doSomething(2)
+//      _ <- interruptible.cancel
     } yield {
       println("finished")
     }
