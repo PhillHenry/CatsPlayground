8e41799478eeb7c016b8ee635bae6245ce680969
Phillip Henry <PhillHenry@gmail.com>
2021-08-14 12:14:16 +0100

    Still struggling with cancellation.

--- src/main/scala/uk/co/odinconsultants/fp/cats/cancellation/WhoWins.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/cancellation/WhoWins.scala
@@ -7,6 +7,12 @@ import cats.implicits._
 
 object WhoWins extends IOApp {
 
+  def javaSleep(ms: Long = 2000) = IO {
+    println(s"About to sleep $ms ms")
+    Thread.sleep(ms)
+    println(s"Finished sleeping $ms ms")
+  }.onCancel(IO.println(s"Sleeping $ms cancelled")).onError(t => IO { t.printStackTrace() })
+
   def print(x: Any): IO[Unit] =
     IO.println(s"$x").onCancel(IO.println(s"$x cancelled")).onError(t => IO {t.printStackTrace()})
 
@@ -19,8 +25,10 @@ object WhoWins extends IOApp {
     val something: IO[Any] = for {
       first <- doSomething(1).start
       _ <- first.cancel
-      interruptible <- IO.interruptible(many = false)(doSomething(3)).start
+      javaSleepFibre <- javaSleep(1999).start
+      interruptible <- IO.interruptible(many = true)(javaSleep()).start
       second <- doSomething(2)
+      _ <- javaSleepFibre.cancel
 //      _ <- interruptible.cancel
     } yield {
       println("finished")
