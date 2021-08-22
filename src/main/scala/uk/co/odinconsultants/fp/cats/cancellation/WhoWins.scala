1691e89f6a48228e5d19cb0caeaac543e0b48f45
Phillip Henry <PhillHenry@gmail.com>
2021-08-22 12:14:05 +0100

    Logging.

--- src/main/scala/uk/co/odinconsultants/fp/cats/cancellation/WhoWins.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/cancellation/WhoWins.scala
@@ -1,50 +1,66 @@
 package uk.co.odinconsultants.fp.cats.cancellation
 
 import cats.effect.{ExitCode, IO, IOApp}
+
 import scala.concurrent.duration.FiniteDuration._
 import scala.concurrent.duration._
 import cats.implicits._
 
+import java.io.{OutputStream, PrintStream}
+
 object WhoWins extends IOApp {
 
-  def javaCode(ms: Long = 2000) = {
-    println(s"About to sleep $ms ms")
-    Thread.sleep(ms)
-    println(s"Finished sleeping $ms ms")
+  val START = System.currentTimeMillis()
+
+  System.setOut(new PrintStream(scala.Console.out) {
+    val OLD_STREAM = System.out
+    override def println(x: String): Unit = OLD_STREAM.println(message(x))
+  })
+
+  def message(x: Any): String = "%5d: %s".format(System.currentTimeMillis() - START, x)
+
+  def javaLog(x: Any): Unit = {
+    println(message(x))
   }
 
-  def javaSleep(ms: Long = 2000): IO[Unit] = logTermination(IO(javaCode(ms)), ms)
+  def javaCode(ms: Long = 2000): Unit = {
+    javaLog(s"About to Thread.sleep($ms)")
+    Thread.sleep(ms)
+    javaLog(s"Finished Thread.sleep($ms)")
+  }
+
+  def javaSleep(ms: Long = 2000): IO[Unit] = logTermination(IO(javaCode(ms)), s"Thread.sleep($ms) cancelled")
 
   def javaSleepInterruptible(ms: Long = 2000): IO[Unit]
-    = logTermination(IO.interruptible(many=true)(javaCode(ms)), ms)
+    = logTermination(IO.interruptible(many=true)(javaCode(ms)), s"IO.interruptible Thread.sleep($ms) cancelled")
 
-  def logTermination(io: IO[Unit], ms: Long = 2000): IO[Unit]
-    = io.onCancel(IO.println(s"Sleeping $ms cancelled")).onError(t => IO { t.printStackTrace() })
+  def logTermination(io: IO[Unit], msg: String): IO[Unit]
+    = io.onCancel(IO.println(msg)).onError(t => IO { t.printStackTrace() })
 
   def print(x: Any): IO[Unit] =
     IO.println(s"$x").onCancel(IO.println(s"$x cancelled")).onError(t => IO {t.printStackTrace()})
 
-  def doSomething(x: Int): IO[Unit] = {
-    val io = print(s"Starting $x") >>
-    IO.sleep(1.seconds).onCancel(IO.println(s"$x sleep cancelled")) >>
-    print(s"Finished $x")
+  def doSomething(x: Any): IO[Unit] = {
+    val io = print(s"Starting '$x'") >>
+      IO.sleep(1.seconds).onCancel(IO.println(s"IO.sleep($x) cancelled")) >>
+      print(s"Finished '$x''")
 
-    io.onCancel(print(s"$x cancelled")) // note the onCancel needs to be on the last IO in the chain
+    io.onCancel(print(s"'$x' cancelled")) // note the onCancel needs to be on the last IO in the chain
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
     val something: IO[Any] = for {
-      first <- doSomething(1).start
+      first <- doSomething("IO.start.cancel").start
       _ <- first.cancel
       javaSleepFibre <- javaSleep(1999).start
       javaInterruptibleFibre <- javaSleepInterruptible(2001).start
       interruptible <- IO.interruptible(many = true)(javaSleep()).start
-      second <- doSomething(2)
+      _ <- doSomething("plain IO.sleep")
       _ <- javaSleepFibre.cancel
       _ <- interruptible.cancel
       _ <- javaInterruptibleFibre.cancel
     } yield {
-      println("finished")
+      javaLog("Terminating...")
     }
     something.as(ExitCode.Success)
   }
