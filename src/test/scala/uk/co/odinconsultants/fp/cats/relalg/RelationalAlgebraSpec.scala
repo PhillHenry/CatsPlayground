93cac8694b8384c9f8e6ac0e0337acfa0bfd0e1c
Phillip Henry <PhillHenry@gmail.com>
2021-08-30 12:41:42 +0100

    Minor refactor.

--- src/test/scala/uk/co/odinconsultants/fp/cats/relalg/RelationalAlgebraSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/relalg/RelationalAlgebraSpec.scala
@@ -13,21 +13,22 @@ class RelationalAlgebraSpec extends CatsEffectSuite {
   val Xs = List(X(1, "a"), X(2, "b"), X(4, "d"))
   val Ys = List(Y(1, 101), Y(2, 102), Y(3, 103))
 
-  type C = (X, Y) => Boolean
-//  case class C(f: C)
+  trait C[X, Y] {
+    def apply(x: X, y: Y): Boolean
+  }
 
-  val primaryKeysMatch: C = (x, y) => x.id == y.id
+  val primaryKeysMatch: C[X, Y] = (x: X, y: Y) => x.id == y.id
 
-  val selectX = IO(Xs)
-  val selectY = IO(Ys)
+  val selectX: IO[List[X]] = IO(Xs)
+  val selectY: IO[List[Y]] = IO(Ys)
 
-  def innerJoinTest(db: RelationalAlgebra[IO, C]): IO[(List[X], List[Y])] = {
-    val join = Join(selectX, selectY, List(primaryKeysMatch))
+  def innerJoinTest(db: RelationalAlgebra[IO, C[X, Y]]): IO[(List[X], List[Y])] = { // TODO bad signature
+    val join: Join[IO, C[X, Y], List[X], List[Y]] = Join(selectX, selectY, List(primaryKeysMatch))
     db.innerJoin(join)
   }
 
-  def innerListJoinTest(db: RelationalAlgebra[List, C]): List[(X, Y)] = {
-    val join: Join[List, C, X, Y] = Join(Xs, Ys, List(primaryKeysMatch))
+  def innerListJoinTest(db: RelationalAlgebra[List, C[X, Y]]): List[(X, Y)] = {
+    val join: Join[List, C[X, Y], X, Y] = Join(Xs, Ys, List(primaryKeysMatch))
     db.innerJoin(join)
   }
 
