ede9b6819e39909f8ac0a4983d1c4f9257e68c6e
Phillip Henry <PhillHenry@gmail.com>
2021-11-20 10:40:17 +0000

    Playing around with type classes.

--- src/main/scala/uk/co/odinconsultants/fp/cats/gvolpe/ImplicitsDemoMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/gvolpe/ImplicitsDemoMain.scala
@@ -15,12 +15,16 @@ object Implication {
 
 object ImplicitsDemoMain {
 
+  /**
+   * No matter what type T is, it is given the extension method.
+   */
   def needsImplication[T: Implication](t: T): String = {
     Implication[T].helloWorld
   }
 
   def main(args: Array[String]): Unit = {
     println(needsImplication(1))
+    println(needsImplication(Option(1)))
   }
 
 }
