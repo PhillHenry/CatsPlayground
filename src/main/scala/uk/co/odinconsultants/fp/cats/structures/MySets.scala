35dd06bd9bcb5493cce602bb926084ba6e447690
Phillip Henry <PhillHenry@gmail.com>
2021-11-21 09:39:48 +0000

    Refactor.

--- src/main/scala/uk/co/odinconsultants/fp/cats/structures/MySets.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/structures/MySets.scala
@@ -12,23 +12,23 @@ class OddEquals(val id: Int, val label: String) {
 
 object MySets {
 
-  type Output = String
   type F = String => OddEquals
-  type G = OddEquals => Output
-
-  def functorLaw(xs: Iterable[String], f: F, g: G): (Iterable[Output], Iterable[Output]) =
-    (xs.map(x => g(f(x))),
-      xs.map(f).map(g))
+  type G = OddEquals => String
 
   val f: F = x => new OddEquals(x.length, x)
   val g: G = _.label
+
+  def functorLaw(xs: Iterable[String], f: F, g: G): (Iterable[String], Iterable[String]) =
+    (xs.map(x => g(f(x))),
+      xs.map(f).map(g))
+
 
   def main(args: Array[String]): Unit = {
     checkFunctorLaws(Set("hello", "world"))
     checkFunctorLaws(List("hello", "world"))
   }
 
-  private def checkFunctorLaws(iterable: Iterable[Output]) = {
+  private def checkFunctorLaws(iterable: Iterable[String]) = {
     val (xs, ys) = functorLaw(iterable, f, g)
     println(s"xs = ${xs}")
     println(s"ys = ${ys}")
