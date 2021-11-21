ce364a9d07bf2bd190c030defba91ec951ffe50b
Phillip Henry <PhillHenry@gmail.com>
2021-11-21 09:23:53 +0000

    Refactor.

--- src/main/scala/uk/co/odinconsultants/fp/cats/structures/MySets.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/structures/MySets.scala
@@ -1,16 +1,13 @@
 package uk.co.odinconsultants.fp.cats.structures
 
-class OddEquals(val x: Int, val y: String) {
-  override def equals(a: Any): Boolean = {
+class OddEquals(val id: Int, val label: String) {
+  override def equals(a: Any): Boolean =
     if (a.isInstanceOf[OddEquals]) {
       val other = a.asInstanceOf[OddEquals]
-      x == other.x
+      id == other.id
     } else false
-  }
 
-  override def hashCode(): Int = {
-    x
-  }
+  override def hashCode(): Int = id
 }
 
 object MySets {
@@ -19,12 +16,12 @@ object MySets {
   type F = String => OddEquals
   type G = OddEquals => Output
 
-  def functorLaw(xs: Iterable[String], f: F, g: G): (Iterable[Output], Iterable[Output]) = {
-    (xs.map(x => g(f(x))), xs.map(f).map(g))
-  }
+  def functorLaw(xs: Iterable[String], f: F, g: G): (Iterable[Output], Iterable[Output]) =
+    (xs.map(x => g(f(x))),
+      xs.map(f).map(g))
 
   val f: F = x => new OddEquals(x.length, x)
-  val g: G = _.y
+  val g: G = _.label
 
   def main(args: Array[String]): Unit = {
     checkFunctorLaws(Set("hello", "world"))
