70556827bb728a30fd1e700050881a6a76dba69e
Phillip Henry <PhillHenry@gmail.com>
2022-06-16 11:25:10 +0100

    Formatting.

--- src/test/scala/uk/co/odinconsultants/fp/cats/relalg/TestSuiteSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/relalg/TestSuiteSpec.scala
@@ -13,22 +13,22 @@ class TestSuiteSpec extends CatsEffectSuite {
     val stream = Stream.constant("hello").take(2).covary[IO].metered(5.second)
 
     val expected = Vector(
-    0.seconds -> Vector(),
-    5.seconds -> Vector("hello"),
-    10.seconds -> Vector("hello", "hello")
+      0.seconds -> Vector(),
+      5.seconds -> Vector("hello"),
+      10.seconds -> Vector("hello", "hello")
     )
 
     // typically this is more complex and has `Ref` and concurrency
     // it should be able to give you the output you assert on without knowing
     // about TestControl
     val harness =
-    IO.monotonic.flatMap { t0 =>
-    stream
-    .scanMap(_.pure[Vector])
-    .evalMap(x => IO.monotonic.map(t1 => t1 - t0).tupleRight(x))
-    .compile
-    .toVector
-  }
+      IO.monotonic.flatMap { t0 =>
+        stream
+        .scanMap(_.pure[Vector])
+        .evalMap(x => IO.monotonic.map(t1 => t1 - t0).tupleRight(x))
+        .compile
+        .toVector
+    }
 
     TestControl.executeEmbed(harness).assertEquals(expected)
   }
