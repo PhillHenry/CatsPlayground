6159d7539953a7c0b2faf658388f1af0acf6e547
Phillip Henry <PhillHenry@gmail.com>
2022-06-16 16:45:29 +0100

    Added code to download.

---
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/pipe/DownloadMain.scala
@@ -0,0 +1,26 @@
+package uk.co.odinconsultants.fp.cats.fs2.pipe
+
+import cats.effect.{ExitCode, IO, IOApp}
+import fs2.{INothing, Pipe, io, Stream}
+import fs2.io.file.Files
+
+import java.io.InputStream
+import java.net.URL
+import java.nio.file.Paths
+
+object DownloadMain extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    download("https://isitchristmas.com/", "/tmp/christmas.txt")
+      .compile.drain.as(ExitCode.Success)
+  }
+
+  def download(spec: String, filename: String): fs2.Stream[IO, Unit] = {
+    val input: InputStream = new URL(spec).openConnection.getInputStream
+    val output: Pipe[IO, Byte, INothing] = Files[IO].writeAll(Paths.get(filename))
+    val stream: fs2.Stream[IO, Byte] = io.readInputStream[IO](IO(input), 4096, closeAfterUse = true)
+//    val tick = Stream.eval(IO(print("."))).repeat
+    stream.through(output)
+  }
+
+}
