e260a8309be452cdc4d97d8b7ab72820be1768d3
Phillip Henry <PhillHenry@gmail.com>
2022-06-17 08:23:37 +0100

    Not incrementing :(

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/pipe/DownloadMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/pipe/DownloadMain.scala
@@ -1,7 +1,7 @@
 package uk.co.odinconsultants.fp.cats.fs2.pipe
 
-import cats.effect.{ExitCode, IO, IOApp}
-import fs2.{INothing, Pipe, io, Stream}
+import cats.effect.{ExitCode, IO, IOApp, Ref}
+import fs2.{INothing, Pipe, Stream, io}
 import fs2.io.file.Files
 
 import java.io.InputStream
@@ -15,12 +15,24 @@ object DownloadMain extends IOApp {
       .compile.drain.as(ExitCode.Success)
   }
 
+  def printTick(i: Int): IO[Unit] = if (i % 10 == 0) IO {
+    print(".")
+  } else IO {
+    print("#")
+  }
+
+  def updating(ref: Ref[IO, Int]): IO[Int] = for {
+    x   <- ref.get
+    _   <- ref.update(_ + 1)
+    _   <- printTick(x)
+  } yield x + 1
+
   def download(spec: String, filename: String): fs2.Stream[IO, Unit] = {
     val input: InputStream = new URL(spec).openConnection.getInputStream
     val output: Pipe[IO, Byte, INothing] = Files[IO].writeAll(Paths.get(filename))
     val stream: fs2.Stream[IO, Byte] = io.readInputStream[IO](IO(input), 4096, closeAfterUse = true)
-    val ticks = Stream.eval(IO(print("."))).repeat
-    stream.zipWith(ticks){case (byte, _) => byte}.through(output)
+    val ticks = Stream.eval(Ref.of[IO, Int](0)).evalMap(updating)
+    stream.zipWith(ticks.repeat){case (byte, _) => byte}.through(output)
   }
 
 }
