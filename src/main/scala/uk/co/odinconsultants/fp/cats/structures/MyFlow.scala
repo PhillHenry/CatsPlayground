351823eacc1ee77b51dd47ee5326837a394f2720
Phillip Henry <PhillHenry@gmail.com>
2022-06-18 13:44:57 +0100

    Merging

--- src/main/scala/uk/co/odinconsultants/fp/cats/structures/MyFlow.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/structures/MyFlow.scala
@@ -1,6 +1,6 @@
 package uk.co.odinconsultants.fp.cats.structures
 
-import cats.{Applicative, ApplicativeError, Id, Monad, MonadError}
+import cats.{Applicative, ApplicativeError, FlatMap, Id, Monad, MonadError}
 import cats.implicits._
 import cats.effect.IO
 import cats.effect.kernel.Sync
@@ -8,52 +8,64 @@ import cats.effect.kernel.Sync
 import scala.util.control.NoStackTrace
 
 sealed trait UserCommand
-case class DownloadCommand(url: String) extends UserCommand
-case class DockerCommand(file: String) extends UserCommand
+case class DownloadCommand(urls: List[String]) extends UserCommand
+case class DockerCommand(file: List[String]) extends UserCommand
 case class DeployCommand(image: String) extends UserCommand
 
 sealed trait CommandResult
-case class DownloadResult(file: String) extends CommandResult
-case class DockerResult(image: String) extends CommandResult
-case class DeployResult(message: String) extends CommandResult
+case class DownloadResult[T[_]](file: T[List[String]]) extends CommandResult
+case class DockerResult[T[_]](image: T[String]) extends CommandResult
+case class DeployResult[T[_]](message: T[String]) extends CommandResult
 
 object CommandError extends NoStackTrace
 
 abstract class Actions[T[_]] {
   def download(url: String): T[String]
-  def docker(file: String):  T[String]
+  def docker(files: List[String]):  T[String]
   def deploy(image: String): T[String]
 }
 
-class Prod[T[_]: MonadError[*[_], Throwable]: Sync] extends Actions[T] {
+class Prod[T[_]: Sync] extends Actions[T] {
   val BAD_URL   = "bad_url"
   val BAD_FILE  = "bad_file"
   val BAD_IMAGE = "bad_image"
 
-  implicit val T = implicitly[Sync[T]]
-
   override def download(url: String): T[String] = delay(url, BAD_URL)
 
-  override def docker(file: String): T[String]  = delay(file, BAD_FILE)
+  def merge(x: T[String], y: T[String]): T[String] = for {
+    a <- x
+    b <- y
+  } yield s"$a, $b"
+
+  override def docker(files: List[String]): T[String]  = {
+    val actions = for { file <- files } yield delay(file, BAD_FILE)
+    actions.reduce((x, y) => merge(x, y))
+  }
 
   override def deploy(image: String): T[String] = delay(image, BAD_IMAGE)
 
-  private def delay(x: String, except: String): T[String] = T.delay(realWork(x, except))
+  private def delay(x: String, except: String): T[String] = {
+    implicit val T = implicitly[Sync[T]]
+    T.delay(realWork(x, except))
+  }
 
-  private def realWork(x: String, except: String): String = {
+  private def realWork(x: String, except: String): String =
     if (x == except)
       throw new Exception()
     else
       x
-  }
 }
 
 object MyFlow {
 
-  def interpreter[T[_]](actions: Actions[T]): UserCommand => T[String] = _ match {
-    case DownloadCommand(url) => actions.download(url)
-    case DockerCommand(file)  => actions.download(file)
-    case DeployCommand(image) => actions.download(image)
+  def interpreter[T[_]: Applicative](actions: Actions[T]): UserCommand => CommandResult = _ match {
+    case DownloadCommand(urls: List[String]) =>
+      val downloads: List[T[String]] = for {
+        url <- urls
+      } yield actions.download(url)
+      DownloadResult(downloads.sequence)
+    case DockerCommand(file)  => DockerResult(actions.docker(file))
+    case DeployCommand(image) => DeployResult(actions.deploy(image))
   }
 
   def main(args: Array[String]): Unit = {
