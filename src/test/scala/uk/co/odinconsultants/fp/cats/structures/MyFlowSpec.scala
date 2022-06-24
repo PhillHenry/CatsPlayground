0f7029b36d9be666cbd658987092581fb3887d53
Phillip Henry <PhillHenry@gmail.com>
2022-06-24 15:22:09 +0100

    MUnit needs to return IOs for them to be executed.

--- src/test/scala/uk/co/odinconsultants/fp/cats/structures/MyFlowSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/structures/MyFlowSpec.scala
@@ -13,15 +13,16 @@ class MyFlowSpec extends CatsEffectSuite {
     val interpreter    = new RetryingInterpreter[IO]
     val externalSystem = new AtomicInteger(0)
     val download       = mockCallToExternalSystem(externalSystem)
-    val results        = interpreter.handleDownloads(List(download)).unsafeRunSync()
-    assertEquals(results, List(PASSED))
-    assertEquals(externalSystem.get(), 2)
+    val results        = interpreter.handleDownloads(List(download))
+    for {
+      _ <- results
+    } yield assertEquals(externalSystem.get(), 2)
   }
 
   private def mockCallToExternalSystem(externalSystem: AtomicInteger): IO[String] = IO {
-      val count = externalSystem.incrementAndGet()
-      print(s"count = $count")
-      if (count == 1) throw CommandError
-      else PASSED
-    }
+    val count = externalSystem.incrementAndGet()
+    print(s"count = $count")
+    if (count == 1) throw CommandError
+    else PASSED
+  }
 }
