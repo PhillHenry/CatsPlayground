4f130b8a20826ea439e0db8b2984f4a691c84dcd
Phillip Henry <PhillHenry@gmail.com>
2022-06-24 16:17:18 +0100

    Clarity.

--- src/test/scala/uk/co/odinconsultants/fp/cats/structures/MyFlowSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/structures/MyFlowSpec.scala
@@ -13,16 +13,14 @@ class MyFlowSpec extends CatsEffectSuite {
     val interpreter    = new RetryingInterpreter[IO]
     val externalSystem = new AtomicInteger(0)
     val download       = mockCallToExternalSystem(externalSystem)
-    val results        = interpreter.handleDownloads(List(download))
     for {
-      _ <- results
+      _ <- interpreter.handleDownloads(List(download))
     } yield assertEquals(externalSystem.get(), 2)
   }
 
   private def mockCallToExternalSystem(externalSystem: AtomicInteger): IO[String] = IO {
     val count = externalSystem.incrementAndGet()
     print(s"count = $count")
-    if (count == 1) throw CommandError
-    else PASSED
+    if (count == 1) throw CommandError else PASSED
   }
 }
