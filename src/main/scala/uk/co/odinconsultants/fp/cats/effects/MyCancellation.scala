36d89f988ec6a64e335861a571443a06a271e3c1
PhillHenry@gmail.com <PhillHenry@gmail.com>
2022-09-02 15:53:42 +0100

    Sleep is interrupted

--- src/main/scala/uk/co/odinconsultants/fp/cats/effects/MyCancellation.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/effects/MyCancellation.scala
@@ -1,5 +1,9 @@
 package uk.co.odinconsultants.fp.cats.effects
-import cats.effect.{IO, IOApp, OutcomeIO, Poll}
+import cats.effect
+import cats.effect.std.Semaphore
+import cats.effect.{IO, IOApp, OutcomeIO, Poll, Resource}
+
+import scala.concurrent.duration._
 
 object MyCancellation extends IOApp.Simple {
 
@@ -16,8 +20,12 @@ object MyCancellation extends IOApp.Simple {
   val failingAcquire: Acquire = poll => logging(s"acquire: $poll") *> failingAt("acquire")
 
   type Use = Resource => IO[Resource]
-  val happyUse: Use   = x => logging(s"use '$x''")
-  val failingUse: Use = x => logging(s"use: $x") *> failingAt("use")
+  val happyUse: Use                         = x => logging(s"use '$x''")
+  val failingUse: Use                       = x => logging(s"use: $x") *> failingAt("use")
+  def waitingUse(wait: FiniteDuration): Use = x =>
+    logging(s"use (waiting): $x") *> IO.sleep(wait) *> IO.println(s"finished waiting $wait") *> IO(
+      x
+    )
 
   type Release = (Resource, OutcomeIO[Resource]) => IO[Unit]
   val happyRelease: Release   = (x, outcome) => logging(s"release $x $outcome").void
@@ -37,14 +45,22 @@ object MyCancellation extends IOApp.Simple {
     val badAcquire =
       IO.bracketFull(failingAcquire)(happyUse)(happyRelease)
 
+    val semaphore                      = Semaphore[IO](1)
+    val res: effect.Resource[IO, Unit] =
+      Resource.make(IO.println("resource acquire"))(x => happyPath *> IO(x))
+
     for {
-      happy     <- happyPath
-      _         <- IO.println("")
-      acquire   <- badAcquire.handleErrorWith(errorHandler)
-      _         <- IO.println("")
-      misuse    <- badUse.handleErrorWith(errorHandler)
-      _         <- IO.println("")
-      releasing <- badRelease.handleErrorWith(errorHandler)
+      happy       <- happyPath
+      _           <- IO.println("")
+      acquire     <- badAcquire.handleErrorWith(errorHandler)
+      _           <- IO.println("")
+      misuse      <- badUse.handleErrorWith(errorHandler)
+      _           <- IO.println("")
+      releasing   <- badRelease.handleErrorWith(errorHandler)
+      _           <- IO.println("")
+      interrupted <- IO.bracketFull(happyPoll)(waitingUse(10.seconds))(happyRelease).start
+      _           <- IO.sleep(1.second)
+      _           <- interrupted.cancel
     } yield ()
   }
 }
