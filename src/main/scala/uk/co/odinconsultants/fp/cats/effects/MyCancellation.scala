0f807d764bd3160a1db0e9b844076f597bb90444
PhillHenry@gmail.com <PhillHenry@gmail.com>
2022-09-03 09:33:35 +0100

    Semaphores and blocking.

--- src/main/scala/uk/co/odinconsultants/fp/cats/effects/MyCancellation.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/effects/MyCancellation.scala
@@ -1,7 +1,8 @@
 package uk.co.odinconsultants.fp.cats.effects
 import cats.effect
+import cats.effect.IO.Blocking
 import cats.effect.std.Semaphore
-import cats.effect.{IO, IOApp, OutcomeIO, Poll, Resource}
+import cats.effect.{IO, IOApp, OutcomeIO, Poll, Resource, Sync}
 
 import scala.concurrent.duration._
 
@@ -23,13 +24,21 @@ object MyCancellation extends IOApp.Simple {
   val happyUse: Use                         = x => logging(s"use '$x''")
   val failingUse: Use                       = x => logging(s"use: $x") *> failingAt("use")
   def waitingUse(wait: FiniteDuration): Use = x =>
-    logging(s"use (waiting): $x") *> IO.sleep(wait) *> IO.println(s"finished waiting $wait") *> IO(
+    logging(s"use (waiting $wait): $x") *> IO {
+      javaWaitFor(wait)
       x
-    )
+    }
 
+  private def javaWaitFor(
+      wait: FiniteDuration
+  ) = {
+    println(s"About to wait $wait")
+    Thread.currentThread().synchronized(Thread.currentThread().wait(wait.toMillis))
+    println(s"finished waiting $wait")
+  }
   type Release = (Resource, OutcomeIO[Resource]) => IO[Unit]
-  val happyRelease: Release   = (x, outcome) => logging(s"release $x $outcome").void
-  val failingRelease: Release = (x, outcome) =>
+  val happyRelease: Release                 = (x, outcome) => logging(s"release $x $outcome").void
+  val failingRelease: Release               = (x, outcome) =>
     logging(s"release $x $outcome") *> failingAt("release").void
 
   override def run: IO[Unit] = {
@@ -45,9 +54,20 @@ object MyCancellation extends IOApp.Simple {
     val badAcquire =
       IO.bracketFull(failingAcquire)(happyUse)(happyRelease)
 
-    val semaphore                      = Semaphore[IO](1)
-    val res: effect.Resource[IO, Unit] =
-      Resource.make(IO.println("resource acquire"))(x => happyPath *> IO(x))
+    val sleepingUse = IO.bracketFull(happyPoll)(waitingUse(3.seconds))(happyRelease)
+
+    val semaphore: IO[Semaphore[IO]]       = Semaphore[IO](1)
+    val res: effect.Resource[IO, Resource] =
+      Resource.makeFull(happyPoll) { x: Resource =>
+        IO.println(s"Releasing $x").void
+      }
+
+    val blockingHappyPath: IO[Resource] = Sync[IO]
+      .blocking {
+        javaWaitFor(3.seconds)
+        "producer.beginTransaction()"
+      }
+      .bracketCase(happyUse)(happyRelease)
 
     for {
       happy       <- happyPath
@@ -58,9 +78,14 @@ object MyCancellation extends IOApp.Simple {
       _           <- IO.println("")
       releasing   <- badRelease.handleErrorWith(errorHandler)
       _           <- IO.println("")
-      interrupted <- IO.bracketFull(happyPoll)(waitingUse(10.seconds))(happyRelease).start
+      interrupted <- sleepingUse.start
       _           <- IO.sleep(1.second)
-      _           <- interrupted.cancel
+      _           <- interrupted.cancel // interrupting makes no difference to the IO
+      _           <- IO.println("")
+      sem         <- Semaphore[IO](1)
+      permit      <- sem.permit.surround(sleepingUse)
+      _           <- IO.println("")
+      blocking    <- blockingHappyPath
     } yield ()
   }
 }
