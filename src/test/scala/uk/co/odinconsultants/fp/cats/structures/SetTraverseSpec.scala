a25f876b8667c9547cb81daca2b28ef4d7d1bbdc
PhillipHenry <PhillHenry@gmail.com>
2023-05-06 11:29:57 +0100

    Superfluous code removed

--- src/test/scala/uk/co/odinconsultants/fp/cats/structures/SetTraverseSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/structures/SetTraverseSpec.scala
@@ -6,48 +6,6 @@ import munit.FunSuite
 
 class SetTraverseSpec extends FunSuite {
   test("traversing sets") {
-    implicit val pAuxSetList: Parallel.Aux[Set, List] = new Parallel[Set] {
-      override def monad: Monad[Set] = ???
-      override type F[x] = List[x]
-
-      override def applicative: Applicative[List] = new Applicative[List] {
-        override def pure[A](x: A): List[A] = List(x)
-        override def ap[A, B](ff: List[A => B])(
-            fa: List[A]
-        ): List[B] = for {
-          fn <- ff
-          a  <- fa
-        } yield fn(a)
-      }
-
-      override def sequential: List ~> Set = new (List ~> Set) {
-        override def apply[A](
-            fa: List[A]
-        ): Set[A] = fa.toSet
-      }
-      override def parallel: Set ~> List   = new (Set ~> List) {
-        override def apply[A](
-            fa: Set[A]
-        ): List[A] = fa.toList
-      }
-    }
-
-    implicit val commAppList = new CommutativeApplicative[List] {
-      override def pure[A](x: A): List[A]                           = List(x)
-      override def ap[A, B](ff: List[A => B])(fa: List[A]): List[B] = for {
-        a <- fa
-        f <- ff
-      } yield f(a)
-    }
-
-    implicit val unorderedTraverseList = new UnorderedTraverse[List] {
-      override def unorderedTraverse[G[_]: CommutativeApplicative, A, B](
-          sa: List[A]
-      )(f: A => G[B]): G[List[B]] = ???
-      override def unorderedFoldMap[A, B: CommutativeMonoid](
-          fa: List[A]
-      )(f: A => B): B = ???
-    }
 
     implicit val trav = new UnorderedTraverse[Set] {
       override def unorderedTraverse[G[_]: CommutativeApplicative, A, B](sa: Set[A])(
